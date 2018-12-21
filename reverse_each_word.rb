3a4806e3194afcc2d6b6eaaa0c7b8ff06b902e1e
Alia B <abadawi712@gmail.com>
2018-12-21 01:52:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word (words)
   words = words.split(" ")
   new_words = []
-  words.each do|words|
+  words.each do|word|
   new_words.push(word.reverse)
 end
-new_words(words)
+new_words(word)
 end
